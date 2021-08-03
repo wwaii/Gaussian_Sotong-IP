df7e8a0013a4020a155798e842ee5aa3f6baac12
wai lun <s10205554@connect.np.edu.sg>
2021-08-03 22:39:12 +0800

    Playfield & Player script added
    new scene "sandbox" added
    c# scripts for plyaer movement and raycast added

---
+++ Assets/Scripts/PlayerRaycasting.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRaycasting : MonoBehaviour
+{
+
+    public Camera playerCamera;
+
+    public int interactionDistance;
+    void Update()
+    {
+        InteractionRaycast();
+    }
+
+    private void InteractionRaycast()
+    {
+        Debug.DrawLine(playerCamera.transform.position, playerCamera.transform.position + playerCamera.transform.forward * interactionDistance);
+        int layerMask = 1 << LayerMask.NameToLayer("IntObject");
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hitInfo, interactionDistance
+            //, layerMask
+            ))
+        {
+            Debug.Log(hitInfo.transform.name);
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+
+            }
+        }
+    }
+}
